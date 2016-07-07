06c3154f6a314393e7dc1ae0de2d49f0dae3b443
Thiren Bunsee <thirenbunsee@gmail.com>
2016-07-07 23:49:39 +0200

    experimenting with winston

--- app.js
+++ app.js
@@ -7,20 +7,47 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 
-winston.add(winston.transports.File, {
-    level: 'info',
-    filename: __dirname + '/logs/logs.log',
-    timestamp: true
-});
+// winston.add(winston.transports.File, {
+//     level: 'info',
+//     filename: __dirname + '/logs/logs.log',
+//     timestamp: true
+// });
 
-winston.log('info', 'Hello log files!');
-winston.info('Hello again log files!');
+// winston.log('info', 'Hello log files!');
+// winston.info('Hello again log files!');
 
 var app = express();
 
 app.disable('x-powered-by');
 
-app.use(morgan('combined', {immediate: true}));
+var logger = new winston.Logger({
+    transports: [
+        new winston.transports.File({
+            level: 'info',
+            filename: './logs/all-logs.log',
+            handleExceptions: true,
+            json: true,
+            maxsize: 5242880, //5MB
+            maxFiles: 5,
+            colorize: false
+        }),
+        new winston.transports.Console({
+            level: 'debug',
+            handleExceptions: true,
+            json: false,
+            colorize: true
+        })
+    ],
+    exitOnError: false
+});
+logger.stream = {
+    write: function (message, encoding) {
+        logger.info(message);
+    }
+};
+
+app.use(morgan('dev', {immediate: true}));
+app.use(morgan('combined', {stream: logger.stream}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
