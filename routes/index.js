488c9ff5aa3630ed79463da8400551d6f3dfca92
Thiren Bunsee <thirenbunsee@gmail.com>
2016-07-17 15:51:26 +0200

    Removed unneeded code

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,8 @@
 
 const express = require('express');
 let router = express.Router();
-let error = require('../utils/error');
 
 router.get('/', function (req, res, next) {
-    let test = error.create();
-
-    console.log(JSON.stringify(test, null, 2));
-
     res.status(200).json({
         api: 'node-api-seed',
         version: '0.0.0'
