9418b1e4902e1d177129a3b1a0329524041b772f
Thiren Bunsee <thirenbunsee@gmail.com>
2017-01-19 15:19:52 +0200

    Added tests for invalid routes

---
+++ test/integration/error-handling/http-404.spec.js
@@ -0,0 +1,26 @@
+const expect = require('chai').expect;
+const request = require('supertest');
+const app = require('../../../app');
+
+describe('Invalid route', function () {
+    it('should return a 404 response', function (done) {
+        request(app)
+            .get('/an-invalid-route')
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            .expect(404)
+            .end(function (err, res) {
+                if (err) {
+                    return done(err);
+                }
+
+                expect(res.body).to.have.property('statusCode');
+                expect(res.body.statusCode).to.equal(404);
+                expect(res.body).to.have.property('error');
+                expect(res.body.error).to.equal('Not Found');
+                expect(res.body).to.have.property('message');
+                expect(res.body.message).to.equal('Route not found');
+                done();
+            });
+    })
+});
